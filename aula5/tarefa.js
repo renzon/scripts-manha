25ffa9b0d50409f1bac8f2f521df8dc2abcb87e4
renzon <renzon@gmail.com>
2015-09-03 07:14:36 -0300

    Aula sobre Herança

--- aula5/tarefa.js
+++ aula5/tarefa.js
@@ -1,3 +1,37 @@
-/**
- * Created by renzo on 8/26/15.
- */
+function construtorObservador() {
+  var lista = [];
+  var evento = {contador:1};
+  var objeto = {
+    contar: function () {
+      lista.forEach(function (valor) {
+        valor(evento);
+      });
+      evento.contador ++;
+    }
+  };
+
+  function adicionarOuvinte(funcao) {
+    lista.push(funcao);
+
+  }
+
+  objeto.adicionarOuvinte = adicionarOuvinte;
+
+  return objeto;
+}
+function observador(evento) {
+  console.log('Contando...' + evento.contador);
+}
+
+function observador2(evento) {
+  console.log('Contando de novo...' + evento.contador);
+}
+
+var contadorObservador = construtorObservador();
+console.log(contadorObservador);
+
+contadorObservador.adicionarOuvinte(observador);
+contadorObservador.adicionarOuvinte(observador2);
+
+contadorObservador.contar();
+contadorObservador.contar();
--- aula6/heranca.js
+++ aula6/heranca.js
@@ -1,18 +1,48 @@
 /**
  * Created by renzo on 8/27/15.
  */
-function Pessoa(nome){
-  this.nome=nome || 'Renzo';
+function Pessoa(nome) {
+  this.nome = nome || 'Renzo';
 }
 
-var pessoaPrototipo={cumprimentar:function(){
-  return 'Olá, meu nome é '+this.nome;
-}};
+var pessoaPrototipo = {
+  cumprimentar: function () {
+    throw 'Método cumprimentar precisa ser implementado';
+  }
+};
 
-Pessoa.prototype=pessoaPrototipo;
+Pessoa.prototype = pessoaPrototipo;
 
-var renzo = new Pessoa();
+function Homem(nome, idade){
+  Pessoa.call(this,nome);
+  this.idade = idade || 18;
+}
+
+var homemPrototipo =new Pessoa();
+homemPrototipo.cumprimentar=function(){
+  return 'Olá '+this.nome;
+};
+homemPrototipo.prototype=Pessoa.prototype;
+Homem.prototype=homemPrototipo;
+
+var renzo = new Homem();
 var roger = new Pessoa('Roger');
 
 console.log(renzo.cumprimentar());
 console.log(roger.cumprimentar());
+
+function Pessoa2(nome) {
+  var obj = {
+    'nome': nome || 'Renzo',
+    cumprimentar: function () {
+      return 'Olá, meu nome é ' + this.nome;
+    }
+  };
+
+  return obj;
+}
+//var estefania = Pessoa2('Estefania');
+//
+//var veronica = Pessoa2('Veronica');
+//console.log(estefania.cumprimentar());
+//console.log(veronica.cumprimentar());
