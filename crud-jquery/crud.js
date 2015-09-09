92861e2bfaa735c95e39f63df0112d7ff6950ec6
renzon <renzon@gmail.com>
2015-09-09 10:45:00 -0300

    Adicionando Categoria na tabela

--- crud-jquery/crud.js
+++ crud-jquery/crud.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
   var $formWell = $('#form-well');
   var $formGroups = $('div.form-group');
   var $helpBlocks = $('span.help-block');
-  var $nomeInput=$('#nome-input');
+  var $nomeInput = $('#nome-input');
+  var $tabelaCategoria = $('#tabela-categoria')
 
   $formWell.hide();
   $('#botao-nova-categoria').click(function () {
@@ -14,21 +15,39 @@ $(document).ready(function () {
     $helpBlocks.text('');
   }
 
-  function mostrarErros(erros){
-    var helpBlockPrefixo='#help-block-';
-    var formGroupPrefixo='#form-group-';
-    for(var propriedade in erros){
-      $(helpBlockPrefixo+propriedade).text(erros[propriedade]);
-      $(formGroupPrefixo+propriedade).addClass('has-error');
+  function adicionarCategoria(categoria) {
+    var linha = '<tr>';
+    linha += '<td>' + categoria.id + '</td>';
+    linha += '<td>' + categoria.creation + '</td>';
+    linha += '<td>' + categoria.nome + '</td>';
+    linha += '<td>';
+    linha += '<button class="btn btn-danger btn-sm"><i class="glyphicon glyphicon-trash"></i></button>';
+    linha += '</td ></tr>';
+
+    $tabelaCategoria.append(linha);
+
+  }
+
+  function mostrarErros(erros) {
+    var helpBlockPrefixo = '#help-block-';
+    var formGroupPrefixo = '#form-group-';
+    for (var propriedade in erros) {
+      $(helpBlockPrefixo + propriedade).text(erros[propriedade]);
+      $(formGroupPrefixo + propriedade).addClass('has-error');
     }
   }
 
-  $('#form-categoria').submit(function(evento){
+  $('#form-categoria').submit(function (evento) {
     evento.preventDefault();
     limparErros();
-    var nome=$nomeInput.val();
-    if(nome===''){
-      mostrarErros({'nome':'Campo Obrigatório'})
+    var nome = $nomeInput.val();
+    if (nome === '') {
+      mostrarErros({'nome': 'Campo Obrigatório'})
+    } else {
+      adicionarCategoria({"id": 5910974510923776,
+        "nome": nome,
+        "creation": "09/08/2015 16:44:20"});
+      $nomeInput.val('');
     }
 
   });
