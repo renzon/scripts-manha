06010c02c05c15f093fd511554a5906e35890e81
renzon <renzon@gmail.com>
2015-09-09 10:27:52 -0300

    Mostrando mensagem de erro

--- crud-jquery/crud.js
+++ crud-jquery/crud.js
@@ -1,21 +1,34 @@
 $(document).ready(function () {
-  var formWell = $('#form-well');
-  var formGroups = $('div.form-group');
-  var helpBlocks = $('span.help-block');
+  var $formWell = $('#form-well');
+  var $formGroups = $('div.form-group');
+  var $helpBlocks = $('span.help-block');
+  var $nomeInput=$('#nome-input');
 
-  formWell.hide();
+  $formWell.hide();
   $('#botao-nova-categoria').click(function () {
-    formWell.slideToggle();
+    $formWell.slideToggle();
   });
 
   function limparErros() {
-    formGroups.removeClass('has-error');
-    helpBlocks.text('');
+    $formGroups.removeClass('has-error');
+    $helpBlocks.text('');
+  }
+
+  function mostrarErros(erros){
+    var helpBlockPrefixo='#help-block-';
+    for(var propriedade in erros){
+      $(helpBlockPrefixo+propriedade).text(erros[propriedade]);
+    }
   }
 
   $('#form-categoria').submit(function(evento){
     evento.preventDefault();
     limparErros();
+    var nome=$nomeInput.val();
+    if(nome===''){
+      mostrarErros({'nome':'Campo Obrigatório'})
+    }
+
   });
 
 });
