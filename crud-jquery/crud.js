18ab09587ca06fafaa265897f48a250693d53d60
renzon <renzon@gmail.com>
2015-09-09 10:07:33 -0300

    Removendo classe de erro

--- crud-jquery/crud.js
+++ crud-jquery/crud.js
@@ -1,9 +1,19 @@
 $(document).ready(function () {
-  var formWell=$('#form-well');
-  formWell.hide();
+  var formWell = $('#form-well');
+  var formGroups = $('.form-group');
 
-  $('#botao-nova-categoria').click(function(){
+  formWell.hide();
+  $('#botao-nova-categoria').click(function () {
     formWell.slideToggle();
-  })
+  });
+
+  function limparErros() {
+    formGroups.removeClass('has-error');
+  }
+
+  $('#form-categoria').submit(function(evento){
+    evento.preventDefault();
+    limparErros();
+  });
 
 });
