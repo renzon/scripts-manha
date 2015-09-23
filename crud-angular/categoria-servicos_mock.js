ab1cb6c3828de7f7438d75de03db6ee41ff5f245
renzon <renzon@gmail.com>
2015-09-23 10:49:54 -0300

    Serviço com chamada assíncrona mock

--- crud-angular/categoria-servicos_mock.js
+++ crud-angular/categoria-servicos_mock.js
@@ -4,8 +4,19 @@
   categoriaServicos.factory('CategoriaAPI', function () {
     return {
       salvar: function (categoria) {
-        console.log('Chamada ajax fake para salvar');
-        console.log(categoria);
+        const delay = 2000;
+        console.log('Chamada ajax fake para salvar inciando');
+        var id = 0;
+        setTimeout(function () {
+          id++;
+          console.log({
+            'id': id,
+            creation: '02/02/02 02:02:02',
+            nome: categoria.nome
+          });
+        }, delay);
+
+        console.log('Finalizando salvar do serviço');
       }
     };
   });
