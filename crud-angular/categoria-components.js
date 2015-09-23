0b85bd45d0508e85c1f56ff60c2216b13a7cb88e
renzon <renzon@gmail.com>
2015-09-23 10:08:24 -0300

    Extração de formulário

--- crud-angular/categoria-components.js
+++ crud-angular/categoria-components.js
@@ -4,7 +4,7 @@
   categoriaComponents.directive('categoriaForm', function () {
     return {
       restric: 'E',
-      template: '<button class="btn btn-danger btn-sm"><i class="glyphicon glyphicon-trash"></i></button>',
+      templateUrl: 'categoria-form.html',
       replace: true
     };
   });
