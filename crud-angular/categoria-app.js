99224132b041724238037688637fc48f8dbd9063
renzon <renzon@gmail.com>
2015-09-23 10:14:23 -0300

    Diretivas sem escopo isolado

--- crud-angular/categoria-app.js
+++ crud-angular/categoria-app.js
@@ -3,6 +3,7 @@
   var categoriaApp = angular.module('categoriaApp', ['categoria-components']);
 
   categoriaApp.controller('CategoriaCtrl', function ($scope) {
+    $scope.categoria = {'nome': 'Veronica'};
 
   });
 })();
