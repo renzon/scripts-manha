1d709e70a83d502dbd1dc142296c8e2d6b09f396
renzon <renzon@gmail.com>
2015-10-14 10:20:37 -0300

    Mock de chamada ajax get

--- tests/categoria-tests.js
+++ tests/categoria-tests.js
@@ -1,3 +1,22 @@
+$.get = function (url) {
+  console.log(url);
+  var getVarsHolder = {
+    'url': url,
+    success: function (callback) {
+      this.successCallback = callback;
+      return this
+    }, error: function (callback) {
+      this.errorCallback = callback;
+      return this
+    }, always: function (callback) {
+      this.alwaysCallback = callback;
+      return this
+    }
+  };
+
+  return getVarsHolder;
+};
+
 QUnit.test('Hello World', function (assert) {
   assert.ok(true, 'Testando método ok');
 });
