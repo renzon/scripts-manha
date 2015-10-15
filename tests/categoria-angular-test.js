af33aca9b0f3e2e2d0c519c453cfe235c5a0de51
renzon <renzon@gmail.com>
2015-10-15 08:48:10 -0300

    Teste com CategoriaAPI

--- tests/categoria-angular-test.js
+++ tests/categoria-angular-test.js
@@ -1,6 +1,111 @@
+var categoriaApi = {
+  deletar: function (id, callbackSucesso, callbackErro, callbackAlways) {
+
+    setTimeout(function () {
+      if (callbackSucesso) {
+
+        callbackSucesso();
+      } else if (callbackErro && categoria.nome === '') {
+        callbackErro({'nome': 'Campo Obrigatório'});
+      }
+
+      if (callbackAlways) {
+        callbackAlways();
+      }
+      $rootScope.$digest();
+    }, delay);
+
+  },
+  editar: function (categoria, callbackSucesso, callbackErro, callbackAlways) {
+
+    setTimeout(function () {
+      if (callbackSucesso && categoria.nome !== '') {
+
+        const categoriaDoServidor = {
+          'id': 1,
+          creation: '02/02/02 02:02:02',
+          nome: categoria.nome
+        };
+        callbackSucesso(categoriaDoServidor);
+      } else if (callbackErro && categoria.nome === '') {
+        callbackErro({'nome': 'Campo Obrigatório'});
+      }
+
+      if (callbackAlways) {
+        callbackAlways();
+      }
+      $rootScope.$digest();
+    }, delay);
+
+  },
+  salvar: function (categoria, callbackSucesso, callbackErro, callbackAlways) {
+
+    console.log('Chamada ajax fake para salvar inciando');
+    var id = 0;
+
+    id++;
+    if (callbackSucesso && categoria.nome !== '') {
+
+      const categoriaDoServidor = {
+        'id': id,
+        creation: '02/02/02 02:02:02',
+        nome: categoria.nome
+      };
+      this.salvarSucesso = function () {
+        callbackSucesso(categoriaDoServidor)
+      };
+    } else if (callbackErro && categoria.nome === '') {
+      this.salvarErro = function () {
+        callbackErro({'nome': 'Campo Obrigatório'})
+      };
+    }
+
+    if (callbackAlways) {
+      this.salvarAlways = callbackAlways;
+    }
+
+
+    console.log('Finalizando salvar do serviço');
+  },
+  listar: function (callbackSucesso, callbackErro, callbackAlways) {
+    setTimeout(function () {
+      if (callbackSucesso) {
+
+        const categoriasDoServidor = [
+          {
+            'id': 1,
+            creation: '02/02/02 02:02:02',
+            nome: 'Celulares'
+          },
+          {
+            'id': 2,
+            creation: '03/03/03 02:02:02',
+            nome: 'Notebooks'
+          },
+          {
+            'id': 3,
+            creation: '04/04/04 02:02:02',
+            nome: 'Tablets'
+          }
+        ];
+        callbackSucesso(categoriasDoServidor);
+      } else if (callbackErro) {
+        callbackErro({'nome': 'Campo Obrigatório'});
+      }
+
+      if (callbackAlways) {
+        callbackAlways();
+      }
+      $rootScope.$digest();
+    }, delay);
+
+    console.log('Finalizando salvar do serviço');
+  }
+};
 angular.module('categoria-servicos', []).factory(
   'CategoriaAPI', function () {
-    return {};
+
+    return categoriaApi;
   });
 angular.module('categoria-components');
 QUnit.test('Teste de Diretiva', function (assert) {
@@ -17,7 +122,14 @@ QUnit.test('Teste de Diretiva', function (assert) {
   var escopoIsolado = el.isolateScope();
 
   assert.strictEqual('Roger', escopoIsolado.categoria.nome, 'Nome inicial de categoria');
+  assert.equal(undefined, escopoIsolado.erros.nome, 'Não possui erros');
   assert.notOk(escopoIsolado.salvandoCategoriaFlag, 'Não está fazendo salvamento');
+
+  escopoIsolado.salvar({'nome': 'Notebooks'});
+  assert.equal(undefined, escopoIsolado.erros.nome, 'Não possui erros');
+  assert.ok(escopoIsolado.salvandoCategoriaFlag, 'Está fazendo salvamento');
+
+  categoriaApi.salvarSucesso();
 
 });
 
