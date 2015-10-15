fbdf76dfcd945fcd27e6e363f23f0d1bd9de5aa3
renzon <renzon@gmail.com>
2015-10-15 07:47:53 -0300

    Testes do controlador

--- tests/categoria-angular-test.js
+++ tests/categoria-angular-test.js
@@ -1,3 +1,31 @@
-/**
- * Created by renzo on 10/15/15.
- */
+QUnit.test('Teste de Controlador Angular', function (assert) {
+
+  angular.module('categoria-components', []);
+  angular.module('categoriaApp');
+
+  var injector = angular.injector(['ng', 'categoriaApp']);
+
+  var escopo = {};
+
+
+  assert.strictEqual(0, escopo.categorias.length, 'Categorias vazio inicialmente');
+  var cat0 = {
+    'id': 1,
+    creation: '02/02/02 02:02:02',
+    nome: 'Celulares'
+  };
+
+  escopo.adicionarCategoria(cat0);
+  assert.strictEqual(1, escopo.categorias.length, 'Uma Categoria adicionada');
+  assert.strictEqual(cat0, escopo.categorias[0], 'Categoria adicionada na primeira posicao');
+  var cat1 = {
+    'id': 2,
+    creation: '03/03/03 02:02:02',
+    nome: 'Notebooks'
+  };
+  escopo.adicionarCategoria(cat1);
+  assert.strictEqual(2, escopo.categorias.length, 'Uma Categoria adicionada');
+  assert.strictEqual(cat0, escopo.categorias[1], 'Categoria movida para segunda posição');
+  assert.strictEqual(cat1, escopo.categorias[0], 'Categoria adicionada na primeira posicao');
+
+});
